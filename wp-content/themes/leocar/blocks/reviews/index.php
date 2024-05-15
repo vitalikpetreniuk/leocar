f0d9285a73b16f14222aa136cbe83931938559bc
alex-gerlakh <oleksandr@ninesquares.studio>
2024-05-15 16:55:30 +0300

    Fix Reviews

--- wp-content/themes/leocar/blocks/reviews/index.php
+++ wp-content/themes/leocar/blocks/reviews/index.php
@@ -4,9 +4,3 @@
         <?php echo do_shortcode('[trustmary_widget id="iN-6LVM_i"]'); ?>
     </div>
 </section>
-<section id="leo-reviews" class="leo-reviews pt-8 mt-16 1100:mt-24">
-    <div class="container">
-        <h2><?php esc_html_e('Відгуки наших клієнтів','leocar'); ?></h2>
-        <?php echo do_shortcode('[trustmary_widget id="iN-6LVM_i"]'); ?>
-    </div>
-</section>
