ad2a24b7e4685135c8f747f7a8159b3c9b41461b
vitalikpetreniuk <petreniuk.ua@gmail.com>
2024-06-06 16:58:17 +0300

    update commit

---
+++ wp-content/themes/leocar/blocks/advantages/index.php
@@ -0,0 +1,16 @@
+<section id="leo-advantages" class="leo-advantages my-0 py-16 1024:mt-32 1024:pb-16 1100:mt-0 1100:pt-48 1100:pb-44">
+    <div class="container">
+        <div>
+            <h2>Підписка дає вам:</h2>
+            <button class="test-drive button mt-5 1024:max-w-[275px] hidden 1100:block">Замовити тест-драйв</button>
+        </div>
+        <ul class="flex flex-col gap-y-2.5 mt-6">
+            <li class="flex justify-start items-center"><span class="block mr-2.5 w-10 min-w-[40px] h-9 rounded-[15px] bg-accent/[.2] flex justify-center items-center"><?php renderAssetsSVG('svg/plus'); ?></span>Користування авто через 29 хв з моменту звернення</li>
+            <li class="flex justify-start items-center"><span class="block mr-2.5 w-10 min-w-[40px] h-9 rounded-[15px] bg-accent/[.2] flex justify-center items-center"><?php renderAssetsSVG('svg/plus'); ?></span>Безлімітний пробіг</li>
+            <li class="flex justify-start items-center"><span class="block mr-2.5 w-10 min-w-[40px] h-9 rounded-[15px] bg-accent/[.2] flex justify-center items-center"><?php renderAssetsSVG('svg/plus'); ?></span>Спокій за технічний стан авто</li>
+            <li class="flex justify-start items-center"><span class="block mr-2.5 w-10 min-w-[40px] h-9 rounded-[15px] bg-accent/[.2] flex justify-center items-center"><?php renderAssetsSVG('svg/plus'); ?></span>Право на викуп зі знижкою</li>
+            <li class="flex justify-start items-center"><span class="block mr-2.5 w-10 min-w-[40px] h-9 rounded-[15px] bg-accent/[.2] flex justify-center items-center"><?php renderAssetsSVG('svg/plus'); ?></span>Можливість виїзду за кордон</li>
+        </ul>
+        <button class="test-drive button mt-6 1024:max-w-[275px] 1100:hidden">Замовити тест-драйв</button>
+    </div>
+</section>
