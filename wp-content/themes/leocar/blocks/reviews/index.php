bb354cd781f6fbebb2c25403cb645d0ecb64365d
vitalikpetreniuk <petreniuk.ua@gmail.com>
2024-07-04 14:40:24 +0300

    main website + landing

--- wp-content/themes/leocar/blocks/reviews/index.php
+++
@@ -1,6 +0,0 @@
-<section id="leo-reviews" class="leo-reviews pt-8 mt-16 1100:mt-24">
-    <div class="container">
-        <h2><?php esc_html_e('Відгуки наших клієнтів','leocar'); ?></h2>
-        <?php echo do_shortcode('[trustmary_widget id="iN-6LVM_i"]'); ?>
-    </div>
-</section>
